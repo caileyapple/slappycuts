d99e07cc4031e86b94ac6a61529cb864b4a5f6b6
caileyapple <122415798+caileyapple@users.noreply.github.com>
2026-03-25 14:22:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function showTab(tabId) {
+  let tabs = document.querySelectorAll(".tab-content");
 
+  tabs.forEach(tab => {
+    tab.classList.remove("active");
+  });
+
+  document.getElementById(tabId).classList.add("active");
